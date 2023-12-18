3c296ad11eb2fabd1d7aec7c781518c190eda030
zoeys-freind <129983142+zoeys-freind@users.noreply.github.com>
2023-12-18 16:33:05 -0600

    Update and rename .travis.yml to setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UltraLink',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Noah Wilhoite',
+    author_email='notnoah349@gmail.com',
+    description='A container for my personal projects.',
+    long_description="""
+# A container for my personal projects.
+- I don't know what else to put here.
+""",
+    long_description_content_type='text/markdown',
+)
